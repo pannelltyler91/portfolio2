8111ee635685bdca4d942403d6cb71cbdf7234b4
Tyler <pannelltyler91@gmail.com>
2024-01-07 16:01:50 +0900

    first commit

---
+++ src/ContactIcons.js
@@ -0,0 +1,10 @@
+
+function ContactIcons(){
+    return(
+        <div>
+            <h1>ContactIcons</h1>
+        </div>
+    )
+}
+
+export default ContactIcons;
---
+++ src/Name.js
@@ -0,0 +1,11 @@
+
+function Name(){
+    return(
+        <div>
+            <h1>Tyler Pannell</h1>
+        </div>
+    )
+    
+}
+
+export default Name;
